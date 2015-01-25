5b381caa3bd90f16d78f4533220e094cbfff257e
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2015-01-25 15:18:12 -0800

    clearer updates

--- launch_on_exception.py
+++ launch_on_exception.py
@@ -1,6 +1,14 @@
-from ipdb import launch_ipdb_on_exception
-x = 40
-with launch_ipdb_on_exception():
-    while 1:
-        x -= 1
-        print(40/x)
+""" launch_ipdb_on_exception demo:
+
+$ ipython
+In [1]: from ipdb import launch_ipdb_on_exception
+In [2]: from launch_on_exception import print_progress
+In [3]: with launch_ipdb_on_exception():
+    ...     print_progress(19, 30)
+"""
+
+
+def print_progress(counter, total):
+    while counter:
+        total -= 1
+        print(float(counter) / total)
